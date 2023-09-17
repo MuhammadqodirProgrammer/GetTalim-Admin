f13fc01054ac726ca5ce4039f6f20354a64292e2
abdulazizdevv <abdulazizkenjaoxunov@gmail.com>
2023-09-17 21:40:51 +0500

    add req

--- src/app/api/api.tsx
+++ src/app/api/api.tsx
@@ -1,48 +1,45 @@
 import axios from "axios";
 const token = localStorage.getItem("token");
-
+export const baseUrl = "http://64.227.42.134:3030";
+export const baseUrlImg = "http://64.227.42.134:3030/api";
 const apiRoot = axios.create({
   baseURL: `http://161.35.188.153`,
   // headers:{
   //   Authorization: `Bearer ${token}`,
   // }
-
 });
 const instance = axios.create({
-    baseURL: "http://64.227.42.134:3030",
-    headers:{
-      ['Authorization']: `Bearer ${token}`,
-    }
+  baseURL: "http://64.227.42.134:3030",
+  headers: {
+    ["Authorization"]: `Bearer ${token}`,
+  },
 });
 
 // Interceptors for handling common scenarios
 instance.interceptors.response.use(
-    response => response,
-    error => {
-        if(error.response.status === 400)
-        {
-            return error.response;
-        }
-        if (error.response.status === 401) {
-            // Redirect to unauthorized page
-            // You can use Vue Router to navigate
-            console.log("401 error handled")
-            alert("Error - 401 Unauthorized")
-        } else if (error.response.status === 404) {
-            // Redirect to not found page
-            // You can use Vue Router to navigate
-            console.log("404 error handled")
-            alert("Error - 404 Not found error")
-        }
-        else if (error.response.status === 500){
-            console.log("500 error handled");
-            alert("Error - 500 Server or Backend")
-        }
-        else {
-            console.log(error.response);
-            alert(error.response.data)
-        }
+  (response) => response,
+  (error) => {
+    if (error.response.status === 400) {
+      return error.response;
     }
+    if (error.response.status === 401) {
+      // Redirect to unauthorized page
+      // You can use Vue Router to navigate
+      console.log("401 error handled");
+      alert("Error - 401 Unauthorized");
+    } else if (error.response.status === 404) {
+      // Redirect to not found page
+      // You can use Vue Router to navigate
+      console.log("404 error handled");
+      alert("Error - 404 Not found error");
+    } else if (error.response.status === 500) {
+      console.log("500 error handled");
+      alert("Error - 500 Server or Backend");
+    } else {
+      console.log(error.response);
+      alert(error.response.data);
+    }
+  }
 );
 
 export default instance;
